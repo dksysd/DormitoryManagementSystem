d0db85a8455cae87716d9661f4c45830cb97db2a
s1hwa <142314061+s1hwa@users.noreply.github.com>
2024-11-13 00:12:19 +0900

    status 등록

--- src/main/java/server/network/protocol/ResponseProtocol.java
+++ src/main/java/server/network/protocol/ResponseProtocol.java
@@ -10,8 +10,27 @@ public class ResponseProtocol extends Protocol {
     @Getter
     @AllArgsConstructor
     public enum Status {
-        OK(200, "OK");
 
+        Continue(100, "Continue"),
+        OK(200, "OK"), Created(201, "Created"),
+        Accepted(202, "Accepted"),
+        Rejected(203, "Rejected"),
+        ResetContent(204, "Reset Content"),
+        PartialContent(205, "Partial Content"),
+        MultiStatus(206, "Multi Status"),
+        MovedPermanently(301, "Moved Permanently"),
+        Found(302, "Found"),
+        NotModified(303, "Not Modified"),
+        BadRequest(400, "Bad Request"),
+        Unauthorized(401, "Unauthorized"),
+        Forbidden(403, "Forbidden"),
+        NotFound(404, "Not Found"),
+        MethodNotAllowed(405, "Method Not Allowed"),
+        NotAcceptable(406, "Not Acceptable"),
+        InternalServerError(500, "Internal Server Error"),
+        BadGateway(502, "Bad Gateway"),
+        ServiceUnavailable(503, "Service Unavailable"),
+        GatewayTimeout(504, "Gateway Timeout");
         private final Integer code;
         private final String message;
     }
