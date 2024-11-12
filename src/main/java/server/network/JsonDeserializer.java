f779f465e1141e0f6886ccb04dfd510c235c8a0d
dksysd <dksysd@kumoh.ac.kr>
2024-11-12 13:36:36 +0900

    직렬화 로직 구현

---
+++ src/main/java/server/network/JsonDeserializer.java
@@ -0,0 +1,180 @@
+package server.network;
+
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JsonDeserializer {
+    private static final ThreadLocal<Map<Integer, Object>> objectMap = new ThreadLocal<>();
+
+    public static <T> T deserialize(String json, Class<T> clazz) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        try {
+            objectMap.set(new HashMap<>());
+            return parseJson(json, clazz);
+        } finally {
+            objectMap.remove();
+        }
+    }
+
+    private static <T> T parseJson(String json, Class<T> clazz) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        JsonParser parser = new JsonParser(json.trim());
+        Object result = parseValue(parser, clazz);
+        return clazz.cast(result);
+    }
+
+    private static Object parseValue(JsonParser parser, Type type) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        parser.skipWhitespace();
+        char current = parser.peek();
+
+        switch (current) {
+            case '{':
+                return parseObject(parser, type);
+            case '[':
+                return parseArray(parser, type);
+            case 't':
+            case 'f':
+                return parser.parseBoolean();
+            case 'n':
+                parser.parseNull();
+                return null;
+            case '"':
+                return parser.parseString();
+            default:
+                if (current == '-' || Character.isDigit(current)) {
+                    return parser.parseNumber();
+                }
+                throw new IllegalArgumentException("Unrecognized token: " + current);
+        }
+    }
+
+    private static Object parseObject(JsonParser parser, Type type) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        parser.expect('{');
+
+        if (!(type instanceof Class<?>)) {
+            throw new IllegalArgumentException("Unrecognized object type: " + type);
+        }
+
+        Class<?> clazz = (Class<?>) type;
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+
+        Object instance = constructor.newInstance();
+
+        parser.skipWhitespace();
+        String fieldName = parser.parseString();
+        parser.expect(':');
+
+        Integer id;
+        if(fieldName.equals("$ref")) {
+            Map<Integer, Object> objectHashMap = objectMap.get();
+            instance = objectHashMap.get((Integer) parseValue(parser, Integer.TYPE));
+            parser.skipWhitespace();
+            if (parser.peek() == '}') {
+                parser.next();
+            }
+            return instance;
+        } else if(fieldName.equals("$id")) {
+            id = (Integer) parseValue(parser, Integer.TYPE);
+        } else {
+            throw new IllegalArgumentException("Object must has rer or id field");
+        }
+
+        parser.skipWhitespace();
+        if (parser.peek() == ',') {
+            parser.next();
+        }
+
+        objectMap.get().put(id, instance);
+
+        Map<String, Field> fields = new HashMap<>();
+        Class<?> currentClass = clazz;
+        while (currentClass != null) {
+            for (Field field : currentClass.getDeclaredFields()) {
+                field.setAccessible(true);
+                fields.put(field.getName(), field);
+            }
+            currentClass = currentClass.getSuperclass();
+        }
+
+        while (true) {
+            fieldName = parser.parseString();
+            parser.expect(':');
+
+            Field field = fields.get(fieldName);
+            if (field == null) {
+                parseValue(parser, Object.class);
+            } else {
+                Object value = parseValue(parser, field.getGenericType());
+                field.set(instance, convertValue(value, field.getType()));
+            }
+
+            parser.skipWhitespace();
+            if (parser.peek() == ',') {
+                parser.next();
+            }
+
+            parser.skipWhitespace();
+            if (parser.peek() == '}') {
+                parser.next();
+                break;
+            }
+        }
+
+        return instance;
+    }
+
+    private static Object parseArray(JsonParser parser, Type type) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        parser.expect('[');
+        List<Object> list = new ArrayList<>();
+
+        Type componentType = Object.class;
+        if (type instanceof ParameterizedType) {
+            componentType = ((ParameterizedType) type).getActualTypeArguments()[0];
+        } else if (type instanceof Class<?> && ((Class<?>) type).isArray()) {
+            componentType = ((Class<?>) type).getComponentType();
+        }
+
+        while (true) {
+            parser.skipWhitespace();
+            if (parser.peek() == ']') {
+                parser.next();
+                break;
+            }
+
+            list.add(parseValue(parser, componentType));
+
+            parser.skipWhitespace();
+            if (parser.peek() == ',') {
+                parser.next();
+            }
+        }
+
+        if (type instanceof Class<?> && ((Class<?>) type).isArray()) {
+            Object array = Array.newInstance((Class<?>) componentType, list.size());
+            for (int i = 0; i < list.size(); i++) {
+                Array.set(array, i, convertValue(list.get(i), (Class<?>) componentType));
+            }
+            return array;
+        }
+        return list;
+    }
+
+    private static Object convertValue(Object value, Class<?> clazz) {
+        if (value == null) return null;
+        if (clazz.isInstance(value)) return value;
+
+        // 숫자 타입 변환
+        if (value instanceof Number num) {
+            if (clazz == Integer.class || clazz == int.class) return num.intValue();
+            if (clazz == Long.class || clazz == long.class) return num.longValue();
+            if (clazz == Double.class || clazz == double.class) return num.doubleValue();
+            if (clazz == Float.class || clazz == float.class) return num.floatValue();
+            if (clazz == Short.class || clazz == short.class) return num.shortValue();
+            if (clazz == Byte.class || clazz == byte.class) return num.byteValue();
+        }
+
+        return value;
+    }
+}
---
+++ src/main/java/server/network/JsonSerializer.java
@@ -0,0 +1,104 @@
+package server.network;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonSerializer {
+    private static final ThreadLocal<Map<Object, Integer>> objectMap = new ThreadLocal<>();
+    private static final ThreadLocal<Map<Integer, Object>> reverseMap = new ThreadLocal<>();
+    private static final ThreadLocal<Integer> counter = new ThreadLocal<>();
+
+    public static String serialize(Object object) throws Exception {
+        try {
+            objectMap.set(new HashMap<>());
+            counter.set(0);
+            return objectToJson(object);
+        } finally {
+            objectMap.remove();
+            counter.remove();
+        }
+    }
+
+    private static String objectToJson(Object object) throws Exception {
+        if (object == null) {
+            return "null";
+        }
+
+        if (object instanceof String) {
+            return "\"" + escapeString((String) object) + "\"";
+        } else if (object instanceof Number || object instanceof Boolean) {
+            return object.toString();
+        } else if (object instanceof Collection) {
+            return collectionToJson((Collection<?>) object);
+        } else if (object instanceof Map) {
+            return mapToJson((Map<?, ?>) object);
+        }
+
+        if (objectMap.get().containsKey(object)) {
+            return "{\"$ref\":" + objectMap.get().get(object) + "}";
+        }
+
+        StringBuilder json = new StringBuilder("{");
+        Integer counterValue = counter.get();
+        json.append("\"$id\":").append(counterValue);
+        objectMap.get().put(object, counterValue++);
+        counter.set(counterValue);
+
+        Field[] fields = object.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(object);
+            json.append(",");
+            json.append("\"").append(field.getName()).append("\":").append(objectToJson(value));
+        }
+
+        return json.append("}").toString();
+    }
+
+    private static String collectionToJson(Collection<?> collection) throws Exception {
+        StringBuilder json = new StringBuilder("[");
+        boolean first = true;
+
+        for (Object item : collection) {
+            if (!first) {
+                json.append(",");
+            }
+            first = false;
+            json.append(objectToJson(item));
+        }
+
+        return json.append("]").toString();
+    }
+
+    private static String mapToJson(Map<?, ?> map) throws Exception {
+        StringBuilder json = new StringBuilder("{");
+        boolean first = true;
+
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            if (!first) {
+                json.append(",");
+            }
+            first = false;
+
+            json.append("\"")
+                    .append(entry.getKey().toString())
+                    .append("\":")
+                    .append(objectToJson(entry.getValue()));
+        }
+
+        return json.append("}").toString();
+    }
+
+    private static String escapeString(String str) {
+        return str.replace("\"", "\\\"")
+                .replace("\\", "\\\\")
+                .replace("\b", "\\b")
+                .replace("\f", "\\f")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
+    }
+}
