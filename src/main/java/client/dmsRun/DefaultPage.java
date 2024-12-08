eca306c2a3807132b8a57d595c80d5516e1716dc
dksysd <dksysd@kumoh.ac.kr>
2024-12-08 23:13:11 +0900

    삭제된 내용 롤백

---
+++ src/main/java/client/dmsRun/DefaultPage.java
@@ -0,0 +1,27 @@
+package client.dmsRun;
+
+import client.auth.Auth;
+import client.domitoryAdmin.AdminPage;
+import client.domitoryUser.ApplicantPage;
+
+public class DefaultPage {
+    public static void run(){
+        //<로그인 화면>
+        //관리자 = 0 , 학생 = 1, 로그인 실패 = -1
+        Auth auth = new Auth();
+        int identity = auth.logIn();
+
+        //<로그인 후 선택하는 화면>
+        if(identity == 0){
+            //관리자 화면
+            AdminPage.adminFunction();
+        }
+        else if(identity == 1){
+            //학생 화면
+            ApplicantPage.applicantFunction();
+        }
+        else {
+            System.out.println("=============로그인 실패!=============");
+        }
+    }
+}
