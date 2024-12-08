e41a66bb6859701829cbeec07850bfbbf187aaac
s1hwa <142314061+s1hwa@users.noreply.github.com>
2024-12-08 16:44:20 +0900

    sessionIdController 커밋

---
+++ src/main/java/server/controller/SessionIdController.java
@@ -0,0 +1,18 @@
+package server.controller;
+
+public class SessionIdController {
+
+
+    public static boolean verifySessionId(String sessionId) {
+
+        //기타 로직 차후 구현
+        return true;
+    }
+
+    public static String getIdByIdBySessionId(String sessionId) {
+        String id = "";
+
+
+        return id;
+    }
+}
