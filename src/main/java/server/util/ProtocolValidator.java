b0c99b1c8db8607be5b6a7e391c7e421c2b8b9c4
s1hwa <142314061+s1hwa@users.noreply.github.com>
2024-12-10 03:04:37 +0900

    UserType 판별 및 학생/관리자 구분 메서드 생성

--- src/main/java/server/util/ProtocolValidator.java
+++ src/main/java/server/util/ProtocolValidator.java
@@ -3,6 +3,8 @@ package server.util;
 
 import server.core.SessionManager;
 
+import java.util.Objects;
+
 public class ProtocolValidator {
 
 
@@ -14,5 +16,14 @@ public class ProtocolValidator {
     public static String getIdBySessionId(String sessionId) {
         return SessionManager.getINSTANCE().getSession(sessionId).getAttribute("ID", String.class);
     }
-
+    public static String getUserType(String sessionId) {
+       return SessionManager.getINSTANCE().getSession(sessionId).getAttribute("UserType", String.class);
+        //UserType은 Student/Admin으로 나뉨
+    }
+    public static boolean isStudent(String sessionId) {
+       return Objects.equals(getIdBySessionId(sessionId), "Student");
+    }
+    public static boolean isAdmin(String sessionId) {
+        return Objects.equals(getIdBySessionId(sessionId), "Admin");
+    }
 }
