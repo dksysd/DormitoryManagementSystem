021175d433a11aa8f67efc9351273fccf8be5933
dksysd <dksysd@kumoh.ac.kr>
2024-12-12 05:02:14 +0900

    종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ dependencies {
     implementation("com.zaxxer:HikariCP:6.0.0")
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
+    implementation("org.slf4j:slf4j-api:1.7.36") // SLF4J API
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0") // Log4j 구현
+    testImplementation("org.slf4j:slf4j-simple:2.1.0-alpha1")
 }
 
 tasks.test {
