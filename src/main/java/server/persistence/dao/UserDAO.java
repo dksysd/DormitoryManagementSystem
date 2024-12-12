c355f9cb9ab668f4ba1557c1145f7657701713b4
Duguda_Study <mjo3482@gmail.com>
2024-12-12 12:40:43 +0900

    ERROR 커버

--- src/main/java/server/persistence/dao/UserDAO.java
+++ src/main/java/server/persistence/dao/UserDAO.java
@@ -38,7 +38,7 @@ public class UserDAO implements UserDAOI {
     public UserDTO findByUid(String uid) throws SQLException {
         String query = "SELECT u.id AS id, u.uid AS uid, u.login_password AS login_password, u.user_name AS user_name, " +
                 "u.phone_number AS phone_number, u.created_at AS created_at, u.updated_at AS updated_at, " +
-                "u.user_type_id AS user_type_id, u.gender_code_id AS gender_code_id, u.address_id AS address_id" +
+                "u.user_type_id AS user_type_id, u.gender_code_id AS gender_code, u.address_id AS address_id" +
                 ", u.profile_image AS profile_image, " +
                 "ut.type_name AS user_type_name, gc.code_name AS code_name, a.detail_address AS user_address, " +
                 "a.postal_name AS postal_code, a.do AS address_do, a.si AS address_si, a.detail_address AS detail_address, " +
