fd14b88f479961c38dde64a3288166915a1942af
Duguda_Study <mjo3482@gmail.com>
2024-12-12 12:50:00 +0900

    ERROR 커버

--- src/main/java/server/persistence/dao/UserDAO.java
+++ src/main/java/server/persistence/dao/UserDAO.java
@@ -14,13 +14,11 @@ public class UserDAO implements UserDAOI {
         String query = "SELECT u.id, u.uid, u.login_password, u.user_name, u.phone_number, u.created_at, u.updated_at, " +
                 "u.user_type_id, u.gender_code_id, u.address_id, u.profile_image," +
                 "ut.type_name AS user_type_name, gc.code_name AS gender_code, a.detail_address AS user_address " +
-                ",a.postal_name AS postal_code, a.do AS address_do, a.si AS address_si, a.detail_address AS detail_address," +
-                "i.name AS image_name, i.extension AS extension " +
+                ",a.postal_name AS postal_code, a.do AS address_do, a.si AS address_si, a.detail_address AS detail_address " +
                 "FROM users u " +
                 "INNER JOIN user_types ut ON u.user_type_id = ut.id " +
                 "INNER JOIN gender_codes gc ON u.gender_code_id = gc.id " +
                 "INNER JOIN addresses a ON u.address_id = a.id " +
-                "INNER JOIN images i ON u.profile_image = i.id " +
                 "WHERE u.id = ?";
         try (Connection connection = DatabaseConnectionPool.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
